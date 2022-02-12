40d1d7fb453ccdda957fede62705bdc924b183b1
Bryan LePla <bdlepla@gmail.com>
2022-02-11 21:26:01 -0500

    added hilt support

---
+++ app/src/main/java/com/bdlepla/android/mymusicplayer/MyMusicApplication.kt
@@ -0,0 +1,9 @@
+package com.bdlepla.android.mymusicplayer
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyMusicApplication: Application() {
+
+}
