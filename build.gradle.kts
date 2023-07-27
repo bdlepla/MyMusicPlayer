240c2412e3b220dd111ddb5024ad9a142d2d23da
Bryan LePla <bdlepla@gmail.com>
2023-07-26 21:29:33 -0400

    More changes for migrating module build gradle file from groovy to kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath ("com.android.tools.build:gradle:8.1.0")
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.40.1")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
+plugins {
+    id ("com.android.application") version "8.1.0" apply false
+    id ("org.jetbrains.kotlin.android") version "1.7.10" apply false
+    //id ("dagger.hilt.android.plugin") version "2.40.1" apply true
 }
