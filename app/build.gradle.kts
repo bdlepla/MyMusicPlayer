04022eeddeee1109843aeb1e0ac0e3476e7a6054
Bryan LePla <bdlepla@gmail.com>
2023-08-24 07:41:12 -0400

    Updated Android Studio Giraffe | 2022.3.1 Patch 1. Update Kotlin to 1.9. Updated used libs to latest.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+//import com.android.build.api.dsl.Packaging
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -51,17 +53,17 @@ android {
     buildFeatures {
         compose = true
     }
-    packagingOptions {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-            excludes += "DebugProbesKt.bin"
-        }
-    }
+//    fun Packaging.() {
+//        resources {
+//            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+//            excludes += "DebugProbesKt.bin"
+//        }
+//    }
     namespace = "com.bdlepla.android.mymusicplayer"
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0-beta01")
+    implementation("androidx.core:core-ktx:1.12.0-rc01")
 
     constraints {
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.0") {
@@ -72,24 +74,24 @@ dependencies {
         }
     }
 
-    implementation("androidx.activity:activity-compose:1.8.0-alpha06")
+    implementation("androidx.activity:activity-compose:1.8.0-alpha07")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.compose.material:material:1.4.3")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha04")
-    implementation("androidx.compose.ui:ui:1.6.0-alpha02")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
-    implementation("androidx.core:core-ktx:1.12.0-beta01")
+    implementation("androidx.compose.material:material:1.5.0")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha06")
+    implementation("androidx.compose.ui:ui:1.6.0-alpha04")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
+    implementation("androidx.core:core-ktx:1.12.0-rc01")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.media:media:1.6.0")
-    implementation("androidx.media3:media3-ui:1.1.0")
-    implementation("androidx.media3:media3-exoplayer:1.1.0")
-    implementation("androidx.media3:media3-session:1.1.0")
+    implementation("androidx.media3:media3-ui:1.1.1")
+    implementation("androidx.media3:media3-exoplayer:1.1.1")
+    implementation("androidx.media3:media3-session:1.1.1")
     //implementation project(":media-lib-ui")
     //implementation project(":media-lib-exoplayer")
     //implementation project(":media-lib-session")
-    implementation("androidx.navigation:navigation-compose:2.6.0")
-    implementation("com.google.android.material:material:1.11.0-alpha01")
+    implementation("androidx.navigation:navigation-compose:2.7.1")
+    implementation("com.google.android.material:material:1.11.0-alpha02")
     implementation("com.google.dagger:hilt-android:2.47")
     implementation("io.coil-kt:coil:2.4.0")
     implementation("io.coil-kt:coil-compose:2.4.0")
@@ -97,10 +99,10 @@ dependencies {
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.3")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.0")
 
-    debugImplementation("androidx.compose.ui:ui-tooling:1.4.3")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-alpha02")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.0")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-alpha04")
 
     testImplementation("junit:junit:4.13.2")
 
