a70098370ab1c1875dc222db1ecd97266b877e10
Bryan LePla <bdlepla@gmail.com>
2023-11-04 17:23:47 -0400

    Updated gradle, kotlin, and libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     compileSdk = 33
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.0"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     defaultConfig {
         //applicationId("com.bdlepla.android.mymusicplayer")
@@ -63,7 +63,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0-rc01")
+    implementation("androidx.core:core-ktx:1.13.0-alpha01")
 
     constraints {
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.0") {
@@ -74,15 +74,15 @@ dependencies {
         }
     }
 
-    implementation("androidx.activity:activity-compose:1.8.0-alpha07")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.compose.material:material:1.5.0")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha06")
-    implementation("androidx.compose.ui:ui:1.6.0-alpha04")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
-    implementation("androidx.core:core-ktx:1.12.0-rc01")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.compose.material:material:1.5.4")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha10")
+    implementation("androidx.compose.ui:ui:1.6.0-alpha08")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
+    implementation("androidx.core:core-ktx:1.13.0-alpha01")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.media:media:1.6.0")
     implementation("androidx.media3:media3-ui:1.1.1")
     implementation("androidx.media3:media3-exoplayer:1.1.1")
@@ -90,19 +90,19 @@ dependencies {
     //implementation project(":media-lib-ui")
     //implementation project(":media-lib-exoplayer")
     //implementation project(":media-lib-session")
-    implementation("androidx.navigation:navigation-compose:2.7.1")
-    implementation("com.google.android.material:material:1.11.0-alpha02")
-    implementation("com.google.dagger:hilt-android:2.47")
-    implementation("io.coil-kt:coil:2.4.0")
-    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
+    implementation("com.google.android.material:material:1.11.0-beta01")
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    implementation("io.coil-kt:coil:2.5.0")
+    implementation("io.coil-kt:coil-compose:2.5.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
 
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.0")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-alpha04")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-alpha08")
 
     testImplementation("junit:junit:4.13.2")
 
