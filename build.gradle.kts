6393d68c3f3511eeb69b0861f63c58da11eedb95
Bryan LePla <bdlepla@gmail.com>
2024-02-29 20:13:07 -0500

    Updated Android Studio and libx

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id ("com.android.application") version "8.2.2" apply false
+    id ("com.android.application") version "8.3.0" apply false
     id ("org.jetbrains.kotlin.android") version "1.9.10" apply false
     //id ("dagger.hilt.android.plugin") version "2.40.1" apply false
     id ("com.google.protobuf") version "0.9.4" apply false
