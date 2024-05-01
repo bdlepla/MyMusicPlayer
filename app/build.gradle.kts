c9f2356a01c2501cbb257e78ce59ba7a8751f4b3
Bryan LePla <bdlepla@gmail.com>
2024-05-01 19:21:58 -0400

    Updated Android Studio. Updated libraries.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,14 +34,14 @@ android {
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro")
         }
         getByName("debug") {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro")
         }
@@ -91,14 +91,14 @@ dependencies {
         }
     }
 
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.compose.material:material:1.6.5")
+    implementation("androidx.compose.material:material:1.6.7")
     implementation("androidx.compose.material3:material3:1.2.1")
-    implementation("androidx.compose.ui:ui:1.6.5")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.6.5")
-    implementation("androidx.core:core-ktx:1.13.0-rc01")
-    implementation("androidx.datastore:datastore-core:1.0.0")
+    implementation("androidx.compose.ui:ui:1.6.7")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.7")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.datastore:datastore-core:1.1.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.media:media:1.7.0")
@@ -120,10 +120,10 @@ dependencies {
     implementation("com.google.android.gms:play-services-cast-framework:21.4.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.5")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.7")
 
-    debugImplementation("androidx.compose.ui:ui-tooling:1.6.5")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.5")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.6.7")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.7")
 
     testImplementation("junit:junit:4.13.2")
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id ("com.android.application") version "8.3.2" apply false
+    id ("com.android.application") version "8.4.0" apply false
     id ("org.jetbrains.kotlin.android") version "1.9.10" apply false
     //id ("dagger.hilt.android.plugin") version "2.40.1" apply false
     id ("com.google.protobuf") version "0.9.4" apply false
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>() {
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     compilerOptions.freeCompilerArgs.addAll(
         "-P",
         "plugin:androidx.compose.compiler.plugins.kotlin:experimentalStrongSkipping=true",
