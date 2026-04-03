f1f70aca1c288a2ec2e64f9e8cd8794594a0e207
bryan <bdlepla@gmail.com>
2026-04-03 19:16:10 -0400

    Fixed build warnings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
